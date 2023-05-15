860033ce89c2cb1a36ab67b029dcb1ccfe516d7f
kevinFAIBM <131728292+kevinFAIBM@users.noreply.github.com>
2023-05-15 17:46:08 -0600

    Primer commit

---
+++ src/Tarea1.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tarea1 {
+    public static void main(String args[]){
+        Map<String,String> nombresCompletos = new HashMap<>();
+        nombresCompletos.put("Kevin","Fallas Alfaro");
+        System.out.println(nombresCompletos);
+    }
+
+}
+
